f14693810da1c9c88da9b43dc7866fe61d75ebc4
KaidenT <89858394+alilgae@users.noreply.github.com>
2024-09-27 17:13:49 -0400

    fight sequence & wrap up

--- Exploring-Variables/Program.cs
+++ Exploring-Variables/Program.cs
@@ -42,8 +42,62 @@
             Console.WriteLine("Boss's health: " + bossHealth);
             Console.WriteLine("Minion 1's health: " + minion1Health);
             Console.WriteLine("Minion 2's health: " + minion2Health);
+            Console.WriteLine();
 
-            
+            //initiative: boss, minion 1, hero, minion 2
+            Console.WriteLine(villianName + " the " + villianTitle + " attacks " + heroFullName);
+            heroHealth -= bossStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+            Console.WriteLine(minionName1 + " attacks " + heroFullName); 
+            heroHealth -= minionStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+            Console.WriteLine(heroFullName + " attacks " + minionName1);
+            minion1Health -= heroStrength;
+            Console.WriteLine(minionName1 + " now has " + minion1Health + " health!");
+            Console.WriteLine(minionName1 + " defeated!");
+
+            Console.WriteLine(heroFullName + " also attacks " + villianName);
+            bossHealth -= heroStrength;
+            Console.WriteLine(villianName + " now has " + bossHealth + " health!");
+
+            Console.WriteLine(minionName2 + " attacks " + villianName + " ...What?");
+            bossHealth -= minionStrength;
+            Console.WriteLine(villianName + " now has " + bossHealth + " health!");
+            Console.WriteLine();
+
+            // end round 1
+            Console.WriteLine(heroFullName + " has " + heroHealth + " health!");
+            Console.WriteLine(heroFullName + " eats an apple to restore health! Yum!");
+            heroHealth++;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+            Console.WriteLine();
+
+            Console.WriteLine(villianName + " has " + bossHealth + " health!");
+            Console.WriteLine(villianName + " drinks a potion to restore health!");
+            bossHealth += 5;
+            Console.WriteLine();
+
+            // round 2 - boss, hero, minion 2
+            Console.WriteLine(villianName + " the " + villianTitle + " attacks " + heroFullName);
+            heroHealth -= bossStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+            Console.WriteLine(heroFullName + " attacks " + minionName2);
+            minion2Health -= heroStrength;
+            Console.WriteLine(minionName2 + " now has " + minion2Health + " health!");
+            Console.WriteLine(minionName2 + " defeated!");
+
+            Console.WriteLine(heroFullName + " also attacks " + villianName);
+            bossHealth -= heroStrength;
+            Console.WriteLine(villianName + " now has " + bossHealth + " health!");
+            Console.WriteLine(villianName + " the " + villianTitle + " defeated!");
+            Console.WriteLine();
+
+            // end round 2
+            Console.WriteLine(heroFullName + " is victorious!");
+            Console.WriteLine("Wealth, glory, and straight A+'s to the winner!");
         }
     }
 }
