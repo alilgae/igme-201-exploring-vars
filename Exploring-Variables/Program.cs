724b9ddc09f4bea5a3773b0282ab12b8e38e647e
KaidenT <89858394+alilgae@users.noreply.github.com>
2024-09-27 16:45:59 -0400

    Began exercise

---
+++ Exploring-Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exploring_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables 
+            string heroFirstName = "Kaiden";
+            string heroLastName = "Terrana";
+            string heroFullName;
+
+            //concat
+
+            heroFullName = heroFirstName + " " + heroLastName;
+
+            Console.WriteLine("Behold, the glorious hero!");
+            Console.WriteLine(heroFullName);
+        }
+    }
+}
