0153d8fe08ad8e24687bc0ba96ee4852fffd28ab
KaidenT <89858394+alilgae@users.noreply.github.com>
2024-09-27 16:58:30 -0400

    fight introductions

--- Exploring-Variables/Program.cs
+++ Exploring-Variables/Program.cs
@@ -9,12 +9,41 @@
             string heroLastName = "Terrana";
             string heroFullName;
 
-            //concat
+            string villianName;
+            string villianTitle = "Master of Shadows";
+
+            string minionTitle = "Placeholder Text";
+            string minionName1;
+            string minionName2;
+
+            int heroHealth = 100, heroStrength = 50;
+            int bossHealth = 100, bossStrength = 25; 
+            int minion1Health = 50, minion2Health = 50, minionStrength = 5;
+
+            //introductions
 
             heroFullName = heroFirstName + " " + heroLastName;
 
-            Console.WriteLine("Behold, the glorious hero!");
-            Console.WriteLine(heroFullName);
+            Console.WriteLine("Behold, the glorious hero, whose name is " + heroFullName);
+
+            villianName = "Steve";
+            minionName1 = "Minion 001";
+            minionName2 = "Minion 002";
+
+            Console.WriteLine(heroFullName + "'s arch rival is " + villianName + " the " + villianTitle);
+
+            Console.WriteLine(villianName + " the " + villianTitle + " is helped by " + 
+                minionTitle + " " + minionName1 + " and " + minionTitle + " " + minionName2);
+
+            //fight! 
+
+            Console.WriteLine("the fighters enter the arena!");
+            Console.WriteLine("Hero's health: " + heroHealth);
+            Console.WriteLine("Boss's health: " + bossHealth);
+            Console.WriteLine("Minion 1's health: " + minion1Health);
+            Console.WriteLine("Minion 2's health: " + minion2Health);
+
+            
         }
     }
 }
